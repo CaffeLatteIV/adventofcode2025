2874f7cf992abf218e1dd3aec895c94762718934
CaffeLatteIV <mattiababbini99@gmail.com>
2025-12-04 07:26:26 +0100

    feat: day 2 progress

--- day2.py
+++ day2.py
@@ -12,17 +12,18 @@ def divisori(num):
             divisors.append(i)  # Add 'i' to the list of divisors
             if i != num // i:
                 divisors.append(num // i)
-    return divisors[1:]
+    return divisors
 
 
 def pattern(p, start, end, size):
-    print(p)
-    val = int(p*size)
+    val = int(str(p)*size)
+    print(f"p {p} val {val} end {end} size {size}")
+    p = int(p)
     if end < val:
         return []
     if start > val:
         return [] + pattern(p+1, start, end, size)
-    return [p] + pattern(p+1, start, end, size)
+    return [val] + pattern(p+1, start, end, size)
 
 
 def genErrors(start, end):
@@ -32,10 +33,12 @@ def genErrors(start, end):
 
     while len(start) != len(end):
         start = '0'+start
-    div = divisori(int(start))
+    div = divisori(len(start))
     if len(div) == 0:
         return []
     for d in div:
+        if d == len(start):
+            continue
         p = pattern(start[:d], int(start), int(end), int(len(start)/d))
         res.append(p)
     return res
