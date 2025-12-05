44d538ee7f6c48b6e29239d8775b070bff1fd91a
CaffeLatteIV <mattiababbini99@gmail.com>
2025-12-05 17:45:28 +0100

    feat: day 3 completed
    Ho creato una stringa composta dai primi size elementi (12 nella parte 2).
    Per verificare se il nuovo numero n potesse generare una stringa dal valore complessivo maggiore di size, ho rimosso l’elemento in posizione i dalla stringa e ho aggiunto n in coda. Ho ripetuto questa operazione per tutti gli elementi della lista e, ogni volta, ho controllato che il valore ottenuto fosse superiore al precedente (o rispetto alla stringa originale).
    Alla fine ho restituito la lista risultante.

--- day3.py
+++ day3.py
@@ -3,20 +3,29 @@ with open('./day3.txt', 'r') as file:
     data = [list(map(int, val.strip())) for val in file.readlines()]
 
 
-res = 0
-for row in data:
-    m = 0
-    pre = 0
-    index = 0
-    for i in range(len(row)):
-        if row[i] > m:
-            m = row[i]
-            index = i
-    for i in range(index+1, len(row)):
+def check(vals, n):
+    res = vals
+    for i in range(len(vals)):
+        val = vals[:i] + vals[i+1:]+str(n)
+        if (int(val) > int(res)):
+            res = val
+    return res
 
-    print(row)
-    print(f"m {m} pre {pre}")
-    print()
-    res += int(str(pre) + str(m))
 
-print(res)
+def joltage(size):
+    res = 0
+    for row in data:
+        vals = ''
+        for i in range(size):
+            vals += str(row[i])
+
+        for i in range(len(vals), len(row)):
+            vals = check(vals, row[i])
+        res += int(vals)
+        print(row)
+        print(vals)
+        print()
+    print(res)
+
+
+joltage(12)
